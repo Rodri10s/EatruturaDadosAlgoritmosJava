4c5cbd3476d8276dd7df9984be26b05abe858279
Ramon Rodrigues <ramonrodrigues838@gmail.com>
2024-04-16 23:00:33 -0300

    Pilha

---
+++ src/pilha/Pilha.java
@@ -0,0 +1,14 @@
+package pilha;
+
+import base.EstruturaEstatica;
+
+public class Pilha <T> extends EstruturaEstatica {
+    
+    public Pilha(){
+        super();
+    }
+
+    public Pilha(int capacidade){
+        super(capacidade);
+    }
+}
