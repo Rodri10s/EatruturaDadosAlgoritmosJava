a4e7cae84187f0698aeb1bb6b2ad00587b494877
Ramon Rodrigues <ramonrodrigues838@gmail.com>
2024-05-28 15:47:27 -0300

    Aula 19 - Introdução Fila

---
+++ src/br/com/estruturadedados/fila/Fila.java
@@ -0,0 +1,13 @@
+package br.com.estruturadedados.fila;
+
+import br.com.estruturadedados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+    public Fila(){
+        super(); //Chama o construtor da classe mãe
+    }
+
+    public Fila(int capacidade){
+        super(capacidade); //Chama o construtor da classe mãe que recebe capacidade
+    }
+}
---
+++ src/br/com/estruturadedados/fila/teste/Aula19.java
@@ -0,0 +1,14 @@
+package br.com.estruturadedados.fila.teste;
+
+import br.com.estruturadedados.fila.Fila;
+
+public class Aula19 {
+    public static void main(String[] args) {
+        
+        Fila<Integer> fila = new Fila<>();
+
+        System.out.println(fila.estaVazio());
+        System.out.println(fila.tamanho());
+
+    }
+}
